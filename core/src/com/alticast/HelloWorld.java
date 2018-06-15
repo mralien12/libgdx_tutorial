83b34ea2b53edd6c4b373b575e145ba8268dabfe
Tran Khanh Hoa <tk.hoa@alticast.com>
2018-06-15 17:24:50 +0700

    Touch gestures

--- core/src/com/alticast/HelloWorld.java
+++ core/src/com/alticast/HelloWorld.java
@@ -2,41 +2,85 @@ package com.alticast;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class HelloWorld implements ApplicationListener, InputProcessor {
+public class HelloWorld implements ApplicationListener, GestureDetector.GestureListener {
 
 	private SpriteBatch batch;
 	private BitmapFont font;
 	private String message = "Touch something already";
 	private int w, h;
 
-	class TouchInfo {
-		public float touchX = 0;
-		public float touchY = 0;
-		public boolean touched = false;
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		return false;
 	}
 
-	private Map<Integer, TouchInfo> touches = new HashMap<Integer, TouchInfo>();
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		message = "Tap performed, finger" + Integer.toString(button);
+		return true;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		message = "Long press performed";
+		return true;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		message = "Fling performed, velocity:" + Float.toString(velocityX) +
+				"," + Float.toString(velocityY);
+		return true;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		message = "Pan performed, delta:" + Float.toString(deltaX) +
+				"," + Float.toString(deltaY);
+		return true;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		message = "Zoom performed, initial Distance:" + Float.toString(initialDistance) +
+				" Distance: " + Float.toString(distance);
+		return true;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+		message = "Pinch performed";
+		return true;
+	}
+
+	@Override
+	public void pinchStop() {
+
+	}
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		font = new BitmapFont(Gdx.files.internal("data/arial-15.fnt"), false);
+		font = new BitmapFont(Gdx.files.internal("data/arial-32.fnt"), false);
 		font.setColor(Color.RED);
 		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
-		Gdx.input.setInputProcessor(this);
-		for (int i =0;i < 5; i++) {
-			touches.put(i, new TouchInfo());
-		}
+
+		GestureDetector gestureDetector = new GestureDetector(this);
+		Gdx.input.setInputProcessor(gestureDetector);
 	}
 
 	@Override
@@ -50,15 +94,6 @@ public class HelloWorld implements ApplicationListener, InputProcessor {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-
-		message = "";
-		for (int i = 0;i<5;i++) {
-			if (touches.get(i).touched) {
-				message += "Finger:" + Integer.toString(i) + "touch  at:" +
-						Float.toString(touches.get(i).touchX) + "," +
-						Float.toString(touches.get(i).touchY) + "\n";
-			}
-		}
 
 		GlyphLayout glyphLayout = new GlyphLayout(font, message);
 		float x = w/2 - glyphLayout.width / 2;
@@ -82,55 +117,5 @@ public class HelloWorld implements ApplicationListener, InputProcessor {
 	@Override
 	public void dispose () {
 
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		if(pointer < 5){
-			touches.get(pointer).touchX = screenX;
-			touches.get(pointer).touchY = screenY;
-			touches.get(pointer).touched = true;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		if(pointer < 5){
-			touches.get(pointer).touchX = 0;
-			touches.get(pointer).touchY = 0;
-			touches.get(pointer).touched = false;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
 	}
 }
