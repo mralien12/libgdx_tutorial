369e20e7612ca9b1e407e2dfcebb2271567b7a74
Tran Khanh Hoa <tk.hoa@alticast.com>
2018-06-15 16:56:57 +0700

    Multitouch

---
+++ desktop/src/com/alticast/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alticast.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alticast.HelloWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloWorld(), config);
+	}
+}
