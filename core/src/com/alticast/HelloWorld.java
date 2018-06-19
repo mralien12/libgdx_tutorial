d2cccd47211c847cd3dbed19ca2ddca28e9aa074
Tran Khanh Hoa <tk.hoa@alticast.com>
2018-06-19 15:02:21 +0700

    Scene2D part 2

--- core/src/com/alticast/HelloWorld.java
+++ core/src/com/alticast/HelloWorld.java
@@ -14,38 +14,42 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
+
+import java.util.Iterator;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
 public class HelloWorld implements ApplicationListener {
 
 	public class MyActor  extends Actor {
 		Texture texture = new Texture(Gdx.files.internal("data/jet.png"));
-		float actorX = 0, actorY  = 0;
 		public boolean started = false;
 
 		public MyActor() {
-			setBounds(actorX, actorY, texture.getWidth(), texture.getHeight());
-			addListener(new InputListener(){
-				public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-					((MyActor) event.getTarget()).started = true;
-					return true;
-				}
-			});
+			setBounds(getX(), getY(), texture.getWidth(), texture.getHeight());
 		}
 
 		@Override
 		public void draw(Batch batch, float parentAlpha) {
-			batch.draw(texture, actorX, actorY);
+			batch.draw(texture,this.getX(),getY(),this.getOriginX(),this.getOriginY(),this.getWidth(),
+					this.getHeight(),this.getScaleX(), this.getScaleY(),this.getRotation(),0,0,
+					texture.getWidth(),texture.getHeight(),false,false);
 		}
 
 		@Override
-		public void act(float delta) {
-			if (started) {
-				actorX += 5;
+		public void act(float delta){
+			for(Iterator<Action> iter = this.getActions().iterator(); iter.hasNext();){
+				iter.next().act(delta);
 			}
 		}
 	}
@@ -57,7 +61,25 @@ public class HelloWorld implements ApplicationListener {
 		Gdx.input.setInputProcessor(stage);
 
 		MyActor myActor = new MyActor();
-		myActor.setTouchable(Touchable.enabled);
+//		SequenceAction sequenceAction = new SequenceAction();
+//
+//		MoveToAction moveAction = new MoveToAction();
+//		RotateToAction rotateAction = new RotateToAction();
+//		ScaleToAction scaleAction = new ScaleToAction();
+//
+//		moveAction.setPosition(300f, 0f);
+//		moveAction.setDuration(5f);
+//		rotateAction.setRotation(90f);
+//		rotateAction.setDuration(5f);
+//		scaleAction.setScale(0.5f);
+//		scaleAction.setDuration(5f);
+//
+//		sequenceAction.addAction(scaleAction);
+//		sequenceAction.addAction(rotateAction);
+//		sequenceAction.addAction(moveAction);
+
+//		myActor.addAction(sequence(scaleTo(0.5f,0.5f,5f),rotateTo(90.0f,5f),moveTo(300.0f,0f,5f)));
+		myActor.addAction(parallel(scaleTo(0.5f,0.5f,5f),rotateTo(90.0f,5f),moveTo(300.0f,0f,5f)));
 		stage.addActor(myActor);
 	}
 
